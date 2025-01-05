b47e09de55302e328de93034212d64de894479a4
Medha <medha2023@gmail.com>
2025-01-05 13:43:06 -0800

    updated photo collage

--- frontend/src/Pages/PhotoGallery/PhotoGallery.jsx
+++ frontend/src/Pages/PhotoGallery/PhotoGallery.jsx
@@ -1,4 +1,5 @@
 import "./PhotoGallery.css";
+import React, { useEffect, useState } from "react";
 import photo1 from "../../Sections/Media/1-photogallery.jpeg";
 import photo2 from "../../Sections/Media/2-photogallery.jpeg";
 import photo3 from "../../Sections/Media/3-photogallery.jpeg";
@@ -11,24 +12,38 @@ import photo9 from "../../Sections/Media/9-photogallery.jpeg";
 import photo10 from "../../Sections/Media/10-photogallery.jpeg";
 import photo11 from "../../Sections/Media/11-photogallery.jpeg";
 import photo12 from "../../Sections/Media/12-photogallery.jpeg";
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 const PhotoGallery = () => {
-    return (
-        <div className="photo-collage">
-            <img src={photo1} alt=""/>
-            <img src={photo2} alt=""/>
-            <img src={photo3} alt=""/>
-            <img src={photo4} alt=""/>
-            <img src={photo5} alt=""/>
-            <img src={photo6} alt=""/>
-            <img src={photo7} alt=""/>
-            <img src={photo8} alt=""/>
-            <img src={photo9} alt=""/>
-            <img src={photo10} alt=""/>
-            <img src={photo11} alt=""/>
-            <img src={photo12} alt=""/>
-        </div>
-    )
-}
+    const [images, setImages] = useState([
+        { src: photo1, alt: "Photo 1" },
+        { src: photo2, alt: "Photo 2" },
+        { src: photo3, alt: "Photo 3" },
+        { src: photo4, alt: "Photo 4" },
+        { src: photo5, alt: "Photo 5" },
+        { src: photo6, alt: "Photo 6" },
+        { src: photo7, alt: "Photo 7" },
+        { src: photo8, alt: "Photo 8" },
+        { src: photo9, alt: "Photo 9" },
+        { src: photo10, alt: "Photo 10" },
+        { src: photo11, alt: "Photo 11" },
+        { src: photo12, alt: "Photo 12" },
+      ]);
+    
+      return (
+            <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3, 1200: 4}}>
+                <Masonry>
+                    {images.map((image, index) => (
+                        <img 
+                            key={index}
+                            alt={image.alt}
+                            src={image.src}
+                            style={{display: "block", padding: "2px", height: "auto"}}
+                        />
+                    ))}
+                </Masonry>
+            </ResponsiveMasonry>
+      );
+    };
 
 export default PhotoGallery;
