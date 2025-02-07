82ca73dfa2fa434df14653756f13ee3015f950c8
Medha <medha2023@gmail.com>
2025-02-06 22:08:26 -0800

    updated look of individual entries page

--- frontend/src/Pages/Experiences/ExpEntries.js
+++ frontend/src/Pages/Experiences/ExpEntries.js
@@ -15,10 +15,15 @@ const ExpEntries = [
             {src: uw_dkdk},
             {src: uw_dkdk2}
         ],
-        context: "COMPANY",
-        responsibilities: "RESPONSIBILITIES",
-        impact: "IMPACT",
-        skills: "SKILLS",
+        context: "CSE 190b is a seminar for students who are direct admits to their computer science major. The seminar is 1.5 hours long for 1x per week and focuses on preparing students for life as a college student and computer science student at the Paul G. Allen School.",
+        responsibilities: "Present a Don't Know what you Don't Know (DKDK) segment focused on teaching a basic level introduction to core CSE topics, such as AI/ML, version control, cybersecurity, quantum computing, and more.",
+        impact: "Able to successfully ensure that 330+ students were comfortable with topics that they may have heard about but never understood or where confused about.",
+        skills: <ul>
+            <li>Presentation</li>
+            <li>Communication</li>
+            <li>Public Speaking</li>
+            <li>Teaching/Instructing</li>
+        </ul>,
         links: [
             {name: "UW CSE190B Course Website", url: "https://sites.google.com/cs.washington.edu/cse-190b-autumn-2024/home"}
         ]
@@ -33,10 +38,13 @@ const ExpEntries = [
         images: [
             {src: uw_ta,}
         ],
-        context: "COMPANY",
+        context: "Snoqualmie Valley School District is based in Snoqualmie, Washington (approximately 30 miles east of Seattle) and is one of the largest school districts in the state, covering more than 400 square miles and serving the communities of Snoqualmie, North Bend, Fall City.",
         responsibilities: "Re-Imaging Devices, Installing Computers and Printers, Asset Verification, Surplus",
-        impact: "IMPACT",
-        skills: "SKILLS",
+        impact: "Successfully re-imaged and prepared 1,500+ student laptops to handoff to incoming freshman and 500+ staff devices. Satisfied 30+ tickets related to classroom technology issues.",
+        skills: <ul>
+            <li>Installing and Updating Computer Software</li>
+            <li>Installing Computer Hardware</li>
+        </ul>,
         links: [
             {name: "Tech Intern Website", url: "https://www.svsd410.org/departments/technology"}
         ] 
--- frontend/src/Pages/Extracurriculars/ExtEntries.js
+++ frontend/src/Pages/Extracurriculars/ExtEntries.js
@@ -9,9 +9,16 @@ const ExtEntries = [
         images: [
         ],
         context: "Student-run organization established to bring inspirational and informative TED style talks to the University of Washington.",
-        responsibilities: "RESPONSIBILITIES",
-        impact: "IMPACT",
-        skills: "SKILLS",
+        responsibilities: <ul>
+            <li>Select and prepare conference's guest speakers.</li>
+            <li>Interview a diverse, engaged, and cohesive pool of speakers whose talks embody the theme ‘Resonance’.</li>
+            <li>Develop speakers' talks and resolve questions and conflicts.</li>
+        </ul>,
+        impact: "Deliver a meaningful and impactful experience to audience with a informative and influential conference.",
+        skills: <ul>
+            <li>Communication</li>
+            <li>Management</li>
+        </ul>,
         links: [
             {name: "Club Website", url: "https://tedxatuofw.com/"}
         ]
@@ -25,12 +32,23 @@ const ExtEntries = [
         description: "Maintaining club website and manage social media with AWS and Git for Applied Analytics Club at UW (AACUW).",
         images: [
         ],
-        context: "COMPANY",
-        responsibilities: "RESPONSIBILITIES",
-        impact: "IMPACT",
-        skills: "SKILLS",
+        context: "An analytics club that focuses on databases & science, modeling & theory, and decisionmaking. Through workshops, guest speakers, networking, and case projects, the club helps members build their analytical skills and prepare for a career with data.",
+        responsibilities: <ul>
+            <li>Maintain the club website.</li>
+            <li>Follow up with project teams.</li>
+        </ul>,
+        impact: <ul>
+            <li>Ensured website functionality and organization, enabling efficient access to resources and reducing operational inefficiencies.</li>
+            <li>Facilitated collaboration and alignment across teams, contributing to a culture of continuous improvement and innovation.</li>
+        </ul>,
+        skills: <ul>
+            <li>AWS</li>
+            <li>Discord</li>
+            <li>Team Management</li>
+            <li>Social Media</li>
+        </ul>,
         links: [
-            {name: "", url: ""}
+            {name: "Club Website", url: "http://aacuw.org/"}
         ] 
     },
     { 
@@ -42,12 +60,20 @@ const ExtEntries = [
         description: "Organizing 3+ impactful fundraisers to raise $1,000+ to support CRY America’s initiatives as part of Child Rights & YOu at UW (CRYUW).",
         images: [
         ],
-        context: "COMPANY",
-        responsibilities: "RESPONSIBILITIES",
-        impact: "IMPACT",
-        skills: "SKILLS",
+        context: "Ensure that basic rights of education, healthcare and protection from exploitation and abuse are restored to underprivileged children by supporting CRY America with fundraisers.",
+        responsibilities: <ul>
+            <li>Plan fundraisers with a team of 3.</li>
+            <li>Work with other teams and faculty of UW to ....</li>
+        </ul>,
+        impact: "Working to raise $500+ to support children's needs and education from other countries.",
+        skills: <ul>
+            <li>Project Planning</li>
+            <li>Teamwork</li>
+            <li>User-Centric Marketing</li>
+        </ul>,
         links: [
-            {name: "", url: ""}
+            {name: "Club Instagram", url: "https://www.instagram.com/cryuw/?hl=en"},
+            {name: "CRY America Website", url: "https://www.cryamerica.org/"}
         ] 
     },
 ];
