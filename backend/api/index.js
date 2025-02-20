4f86f31eb99da38ec44d40c713dc1e64eae26c03
Medha <medha2023@gmail.com>
2025-02-20 13:41:22 -0800

    update backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,36 @@
-import app from "../server.js";
+// import app from "../server.js";
+
+// export default app;
+
+import emailRouter from "./api/routes/emailRoute.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.use(cors({
+    origin: 'https://srimedha-thummala.vercel.app'
+}));
+
+app.use("/api/sendemail/", emailRouter);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// app.listen(PORT, (error) => {
+//     if (!error) {
+//         console.log("Server Running on Port " + PORT);
+//     } else {
+//         console.log("Error: " + error);
+//     }
+// });
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
-import emailRouter from "./routes/emailRoute.js";
-import express from "express";
-import dotenv from "dotenv";
-import cors from 'cors';
+// import emailRouter from "./api/routes/emailRoute.js";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from 'cors';
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-const PORT = 3000;
+// const app = express();
+// const PORT = 3000;
 
-app.use(express.json());
+// app.use(express.json());
 
 
-app.use(cors({
-    origin: 'https://srimedha-thummala.vercel.app'
-}));
+// app.use(cors({
+//     origin: 'https://srimedha-thummala.vercel.app'
+// }));
 
-app.use("/api/sendemail/", emailRouter);
+// app.use("/api/sendemail/", emailRouter);
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+// app.get('/', (req, res) => {
+//     res.send('API is running...');
+// });
 
-app.listen(PORT, (error) => {
-    if (!error) {
-        console.log("Server Running on Port " + PORT);
-    } else {
-        console.log("Error: " + error);
-    }
-});
+// app.listen(PORT, (error) => {
+//     if (!error) {
+//         console.log("Server Running on Port " + PORT);
+//     } else {
+//         console.log("Error: " + error);
+//     }
+// });
 
-export default app;
+// export default app;
